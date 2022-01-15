25d862f0fbfead6dc414647d43ca1842dc1cfec2
sunny7dusk <nat233947@gmail.com>
2022-01-15 18:46:52 -0500

    Attemp 4

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { HelmetProvider } from 'react-helmet-async';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </HelmetProvider>
   </React.StrictMode>,
   document.getElementById('root'),
